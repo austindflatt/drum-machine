e7c5576b801b7ce435e0704c8686d0dd23849f63
Austin Flatt <austindflatt@gmail.com>
2021-11-16 16:06:27 -0500

    toggling instruments done

--- main.js
+++ main.js
@@ -7,20 +7,37 @@ const snareDrum = new Audio('sounds/snare-drum.mp3');
 
 // This function is called every 600ms
 function update() {
+    setTick = 0;
     const metronomeCount = document.querySelector('.metronomeCount');
+    const metronome = document.querySelector('#metronome');
+    const hiHatId = document.querySelector('#hi-hat');
+    const kickDrumId = document.querySelector('#kick-drum');
+    const snareDrumId = document.querySelector('#snare-drum');
+    const math = (setTick % 4) + 1;
 
-    // Play the 'tick' sound
-    tick.play();
-}
-
-function secondSound () {
-    tock.play();
+    setTick++;
+    if (metronome.checked) {
+        if (setTick % 4 === 0) {
+            tock.play();
+        } else {
+            // Play the 'tick' sound
+            tick.play();
+        }
+    }
+    if (hiHatId.checked) {
+        hiHat.play();
+    }
+    if (snareDrumId.checked) {
+        snareDrum.play();
+    }
+    if (kickDrumId.checked) {
+        kickDrum.play();
+    }
 }
 
 // This function sets up update() to be called every 600ms
 function setupUpdate() {
     setInterval(update, 600);
-    setInterval(secondSound, 2600);
 }
 
 // Call setupUpdate() once after 300ms
