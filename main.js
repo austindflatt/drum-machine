46441a54f95dbcd0acd3451a72d3ff9d1d822421
Austin Flatt <austindflatt@gmail.com>
2021-11-17 14:12:00 -0500

    update completed

--- main.js
+++ main.js
@@ -5,17 +5,26 @@ const hiHat = new Audio('sounds/hi-hat.mp3');
 const kickDrum = new Audio('sounds/kick-drum.mp3');
 const snareDrum = new Audio('sounds/snare-drum.mp3');
 
+let setTick = 0;
+
 // This function is called every 600ms
 function update() {
-    setTick = 0;
     const metronomeCount = document.querySelector('.metronomeCount');
     const metronome = document.querySelector('#metronome');
     const hiHatId = document.querySelector('#hi-hat');
+    const hiHatInput = document.querySelector('#hihat-number');
     const kickDrumId = document.querySelector('#kick-drum');
+    const kickDrumInput = document.querySelector('#kick-drum-number');
     const snareDrumId = document.querySelector('#snare-drum');
+    const snareDrumInput = document.querySelector('#snare-drum-number');
     const math = (setTick % 4) + 1;
 
     setTick++;
+    if (setTick < 5) {
+        metronomeCount.innerText = setTick;
+    } else {
+        setTick = 0;
+    }
     if (metronome.checked) {
         if (setTick % 4 === 0) {
             tock.play();
@@ -25,13 +34,19 @@ function update() {
         }
     }
     if (hiHatId.checked) {
-        hiHat.play();
+        if (Number(hiHatInput.value) === math) {
+            hiHat.play();
+        }
     }
     if (snareDrumId.checked) {
+        if (Number(snareDrumInput.value) === math) {
         snareDrum.play();
+        }
     }
     if (kickDrumId.checked) {
-        kickDrum.play();
+        if (Number(kickDrumInput.value) === math) {
+            kickDrum.play();
+        }
     }
 }
 
